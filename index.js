295a4b17d25eeffdb11f86615033d67736bbba8e
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-08-18 12:02:04 +0430

    const oauth2client created

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const {google} = require('googleapis');
+require('dotenv').config();
+
+const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRCET_URI,
+  );
 
 app.get('/',(req,res) => {
     res.send('API is running successfuly!');
