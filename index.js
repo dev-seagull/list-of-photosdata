379a0d78c50f02aa940598cbf211384d377226b6
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-08-18 11:19:08 +0430

    Running the webserver on local host

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = express();
+
+app.get('/',(req,res) => {
+    res.send('API is running successfuly!');
+})
+
+app.listen(7777, () => console.log('Server is listening to port 7777'));
