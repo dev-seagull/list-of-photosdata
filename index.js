e87ecf4836c50d521df8e3035f515b555cf1e73c
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-08-18 12:05:27 +0430

    Adding scopes of Google Photos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const {google} = require('googleapis');
+const Photos = require('googlephotos');
 require('dotenv').config();
 
 const oauth2Client = new google.auth.OAuth2(
@@ -8,6 +9,8 @@ const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_SECRET,
     process.env.REDIRCET_URI,
   );
+
+  const scopes = [Photos.Scopes.READ_ONLY, Photos.Scopes.SHARING];
 
 app.get('/',(req,res) => {
     res.send('API is running successfuly!');
