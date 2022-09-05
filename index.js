639c9aa1221ba5d0c25deb5e9dac1d75ec3ed06e
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-09-06 00:57:01 +0430

    Debugging

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const {google} = require('googleapis');
-const { analytics } = require('googleapis/build/src/apis/analytics');
 const Photos = require('googlephotos');
 require('dotenv').config();
+
+//const https = require('https');
+//const download = require('image-downloader');
+//const fs = require('fs');
 
 const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
@@ -11,8 +14,22 @@ const oauth2Client = new google.auth.OAuth2(
     process.env.REDIRCET_URI,
   );
 
-const photos = new Photos(process.env.ACCESS_TOKEN);
+//const scopes = [Photos.Scopes.READ_ONLY, Photos.Scopes.SHARING];
 
+//const url = oauth2Client.generateAuthUrl({
+  // 'online' (default) or 'offline' (gets refresh_token)
+  //access_type: 'offline',
+
+  // If you only need one scope you can pass it as a string
+  //scope: scopes,
+//});
+
+//console.log(url);
+
+//const {tokens} = oauth2Client.getToken(code);
+
+
+const photos = new Photos(process.env.ACCESS_TOKEN);
 
 async function listMediaItems(){
   try{
@@ -25,9 +42,25 @@ async function listMediaItems(){
 
 listMediaItems();
 
+//function downloadImage(url,filepath){
+  //https.get(url, (res) => { 
+    //res.pipe(fs.createWriteStream(filepath));
+  //});
+//}
+
+//function Downloadd(url,filepath){
+  //return download.image({
+    //url,
+    //dest: filepath
+  //})
+//}
+
+//const url = 'https://en.wikipedia.org/wiki/Hello'
+//const filepath = 'F:/computerscience/Seagull/GooglePhotosAPI/Images'
+//downloadImage(url,filepath);
+//Downloadd(url,filepath)
+
 app.get('/',(req,res) => {
     res.send('API is running successfuly!');
 })
-
-
 app.listen(7777, () => console.log('Server is listening to port 7777'));
