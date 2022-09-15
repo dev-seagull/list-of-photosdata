3d892919b540cc86972e4ec6d81fe37b4722980d
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-09-15 12:41:57 +0430

    Commenting unnecessary parts and a for lopp in order to get all the mediametadatas instead of one

--- index.js
+++ index.js
@@ -5,19 +5,13 @@ const Photos = require('googlephotos');
 require('dotenv').config();
 
 
-//const https = require('https');
-//const download = require('image-downloader');
-//const fs = require('fs');
-
-const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRCET_URI,
-  );
-
+//const oauth2Client = new google.auth.OAuth2(
+//    process.env.CLIENT_ID,
+//    process.env.CLIENT_SECRET,
+//    process.env.REDIRCET_URI,
+//  );
 // photoslibrary.readonly
 //const scopes = [Photos.Scopes.READ_ONLY, Photos.Scopes.SHARING];
-
 //const url = oauth2Client.generateAuthUrl({
   // 'online' (default) or 'offline' (gets refresh_token)
   //access_type: 'offline',
@@ -25,13 +19,12 @@ const oauth2Client = new google.auth.OAuth2(
   // If you only need one scope you can pass it as a string
   //scope: scopes,
 //});
-
 //console.log(url);
-
 //const {tokens} = oauth2Client.getToken(code);
 
 
 const photos = new Photos(process.env.ACCESS_TOKEN);
+
 
 async function listMediaItems(){
   try{
@@ -39,9 +32,11 @@ async function listMediaItems(){
     console.log("This is the list of media items: ")
     console.log(mediaItemsList)
     console.log("\n")
-    console.log("This is the MediaMetadata of the first mediaitem: ")
+    console.log("This is the MediaMetadata of the mediaitems: ")
     console.log("\n")
-    console.log(mediaItemsList[ 'mediaItems' ][0].mediaMetadata)
+    for(let i=0; i< Object.keys(mediaItemsList["mediaItems"]).length ;i++){
+      console.log(mediaItemsList["mediaItems"][i]["mediaMetadata"])
+    }
   } catch(error){
     console.log(error.message)
   }
@@ -58,19 +53,6 @@ async function listAlbums(){
   }
 } 
 
-//async function getAlbumIds(){
-  //try{
-    //const albumsList = await photos.albums.list()
-    //const AlbumIds = []
-    //for(let i=0; i< Object.keys(albumsList["albums"]).length ;i++){
-      //AlbumIds[i] = albumsList[ 'albums' ][i]["id"]
-      //console.log(AlbumIds[i])
-    //}
-    //return AlbumIds
-  //}catch(error){
-    //console.log(error.message)
-  //}
-//}
 
 async function listAlbumContents(){
   try{
@@ -90,11 +72,12 @@ async function listAlbumContents(){
 
 
 listMediaItems();
-
 listAlbums();
-
 listAlbumContents();
 
+//const https = require('https');
+//const download = require('image-downloader');
+//const fs = require('fs');
 //function downloadImage(url,filepath){
   //https.get(url, (res) => { 
     //res.pipe(fs.createWriteStream(filepath));
@@ -113,14 +96,6 @@ listAlbumContents();
 //downloadImage(url,filepath);
 //Downloadd(url,filepath)
 
-//app.get('/',(req,res) => {
-  //  res.send('API is running successfuly!');
-//})
-
-//app.listen(7777, () => console.log('Server is listening to port 7777'));
-
-//const albumsList = JSON.stringify(listAlbums());
-//console.log(albumsList);
 
 //photos.mediaItems.batchGet
 //enrichment
